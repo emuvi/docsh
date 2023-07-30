72aa90388724880c7dc78bbdde85e19aae71c377
Éverton M. Vieira <emuvi@outlook.com.br>
2023-07-30 17:53:47 -0300

    setup the basic gui interface

--- src/main/java/br/com/pointel/docsh/gui/Gui.java
+++ src/main/java/br/com/pointel/docsh/gui/Gui.java
@@ -1,6 +1,13 @@
 package br.com.pointel.docsh.gui;
 
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
@@ -9,13 +16,35 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 
 public class Gui extends JFrame {
 
+    private final JPanel panelRoot = new JPanel(new GridLayout(3, 2));
+    private final JLabel labelPath = new JLabel("Path:", JLabel.RIGHT);
+    private final JTextField fieldPath = new JTextField(18);
+    private final JLabel labelSearch = new JLabel("Search:", JLabel.RIGHT);
+    private final JTextField fieldSearch = new JTextField(18);
+    private final JLabel labelStatus = new JLabel("", JLabel.RIGHT);
+    private final JButton buttonStart = new JButton("Start");
+    
+
     public Gui() {
         initComponents();
     }
 
     private void initComponents() {
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Docsh");
+        setContentPane(panelRoot);
+        panelRoot.add(labelPath);        
+        panelRoot.add(fieldPath);
+        panelRoot.add(labelSearch);
+        panelRoot.add(fieldSearch);
+        panelRoot.add(labelStatus);
+        panelRoot.add(buttonStart);
+        var border = BorderFactory.createEmptyBorder(9, 9, 9, 9);
+        panelRoot.setBorder(border);
+        labelPath.setBorder(border);
+        labelSearch.setBorder(border);
+        labelStatus.setBorder(border);
         pack();
     }
 
